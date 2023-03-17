4eb94cd52ba7ff43fb0258d39654718789b85728
Rahil Khan <hi.rahilkhan@gmail.com>
2023-03-17 22:54:01 +0530

    arrangement

---
+++ src/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import head from './head'
+import footer from './footer'
+const home = () => {
+  return (
+    <>
+    </head>
+    <div>home</div>
+    </footer>
+    </> 
+     )
+}
+
+export default home
